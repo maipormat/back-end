fb81086869dcf97fc01485c9b2e85f425cf4776d
maipormat <145042299+maipormat@users.noreply.github.com>
2024-12-02 14:28:56 +0700

    Update server.js

--- server.js
+++ server.js
@@ -21,29 +21,29 @@ connectCloudinary();
 
 // middlewares
 app.use(express.json());
-const allowedOrigins = [
-  "https://vercel.com/bananas-projects-9860d87b/admin-test",
-  "http://localhost:5175", // For local development
-  "http://localhost:5176", // For local development
-  "http://localhost:5173", // For local development
-];
+// const allowedOrigins = [
+//   "https://vercel.com/bananas-projects-9860d87b/admin-test",
+//   "http://localhost:5175", // For local development
+//   "http://localhost:5176", // For local development
+//   "http://localhost:5173", // For local development
+// ];
 
-// Configure CORS
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      // Allow requests with no origin (e.g., mobile apps or curl)
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.includes(origin)) {
-        callback(null, true); // Allow the origin
-      } else {
-        callback(new Error("Not allowed by CORS")); // Block the origin
-      }
-    },
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true, // Allow cookies or Authorization headers
-  })
-);
+// // Configure CORS
+// app.use(
+//   cors({
+//     origin: (origin, callback) => {
+//       // Allow requests with no origin (e.g., mobile apps or curl)
+//       if (!origin) return callback(null, true);
+//       if (allowedOrigins.includes(origin)) {
+//         callback(null, true); // Allow the origin
+//       } else {
+//         callback(new Error("Not allowed by CORS")); // Block the origin
+//       }
+//     },
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     credentials: true, // Allow cookies or Authorization headers
+//   })
+// );
 
 // api endpoints
 app.use("/api/user", userRouter);
